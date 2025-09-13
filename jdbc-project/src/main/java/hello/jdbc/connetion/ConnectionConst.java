691ee2e6796cadc1e43a3b945fa1a8a5d6bd39b9
zkvn1103@naver.com <zkvn1103@naver.com>
2025-09-13 18:05:29 +0900

    jdbc 연결 및 테스트

---
+++ jdbc-project/src/main/java/hello/jdbc/connetion/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbc.connetion;
+
+public abstract class ConnectionConst {
+  public static final String URL = "jdbc:mariadb://localhost:3306/charly_market";
+  public static final String USERNAME = "root";
+  public static final String PASSWORD = "als435125";
+}
