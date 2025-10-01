85965771e5ecf1fdf6b50a219915c9ff4ef68144
zkvn1103@naver.com <zkvn1103@naver.com>
2025-10-01 11:41:12 +0900

    rename

--- project/src/main/java/hello/servlet/connetion/ConnectionConst.java
+++
@@ -1,7 +0,0 @@
-package hello.servlet.connetion;
-
-public abstract class ConnectionConst {
-  public static final String URL = "jdbc:mariadb://localhost:3306/charly_market";
-  public static final String USERNAME = "root";
-  public static final String PASSWORD = "als435125";
-}
